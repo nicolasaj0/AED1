c984426db49567d7b727542a42ff22ddf074af25
Nicolas <52798064+nicolasaj0@users.noreply.github.com>
2024-09-18 17:19:17 -0300

    Números naturais
    Imprimindo números naturais recursivamente

---
+++ Recursividade/exercicio_01.c
@@ -0,0 +1,16 @@
+//Exercicio 1 Recursividade
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        printf(" %d", i);
+    }
+
+    return 0;
+}
